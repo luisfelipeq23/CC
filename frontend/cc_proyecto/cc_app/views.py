6adbddead1ff6df29b58ffba32a08d765d332bc4
luisfelipeq23 <lfqueram@upv.edu.es>
2023-01-25 02:36:19 +0100

    aniadido a views.py envío de mensajes a Kafka

--- frontend/cc_proyecto/cc_app/views.py
+++ frontend/cc_proyecto/cc_app/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 from .models import Repo
 from .forms import RepoForm
 
+from kafka import KafkaProducer
+
 def home(request):
     return render(request, 'home.html')
 
@@ -14,7 +16,15 @@ def download_repo(request):
             repo_name=form.cleaned_data['nombre_repo']
             repo_url=form.cleaned_data['url_repo']
             repo = Repo(nombre_repo=repo_name, url_repo=repo_url)
+            # Salvar repositorio a BD
             repo.save()
+            # Envío de mensaje a Kafka
+            enviar_mensaje_kafka(repo_name, repo_url)
             return render(request, 'RepoForm.html', {'form': RepoForm()})
-        return render(request, 'RepoForm.html', {'form': form, "error": "Completar los datos requeridos"})
+        return render(request, 'RepoForm.html', {'form': form, "error": "Complete los datos requeridos"})
     return render(request, 'RepoForm.html', {'form': form})
+
+def enviar_mensaje_kafka(repo_name, repo_url):
+    mensaje = {repo_name:repo_url}
+    productor = KafkaProducer(bootstrap_servers="cc_kafka:9092")
+    productor.send("pendientes", mensaje)
